b32152192ff6cd8e5e536f9387bd0d7ea80387e2
MaxSch73 <schuller.m04@htlwienwest.at>
2023-02-15 14:28:36 +0100

    Icons

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 import { VitePWA } from 'vite-plugin-pwa';
+import manifest from './manifest';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
@@ -13,7 +14,7 @@ export default defineConfig({
       template: { transformAssetUrls },
     }),
 
-    VitePWA({ registerType: 'autoUpdate' }),
+    VitePWA({ registerType: 'autoUpdate', manifest }),
     quasar({
       sassVariables: 'src/quasar-variables.sass',
     }),
