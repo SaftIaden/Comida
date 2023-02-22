a44f76ec7b0191c4358b48e4887fd7056199b4e2
MaxSch73 <schuller.m04@htlwienwest.at>
2023-02-22 10:58:44 +0100

    CameraLoop

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import LikeView from '../views/LikeView.vue';
-import HomeView from '../views/HomeView.vue';
+import CameraView from '../views/CameraView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +15,11 @@ const router = createRouter({
       path: '/likes',
       name: 'likes',
       component: LikeView,
+    },
+    {
+      path: '/camera',
+      name: 'camera',
+      component: CameraView,
     },
   ],
 });
---
+++ client/src/views/CameraView.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-page class="q-pa-md">
+    <Camera
+      @loading="loading"
+      @started="started"
+      @stopped="stopped"
+      @paused="paused"
+      @resumed="resumed"
+      @camera-change="cameraChange"
+      @snapshot="snapshot"
+      @error="error"
+      ref="camera"
+      :resolution="{ width: 375, height: 812 }"
+      autoplay
+    >
+    </Camera>
+    <button @click="snapshot">Create snapshot</button>
+  </q-page>
+</template>
+
+<script setup>
+import Camera from 'simple-vue-camera';
+import { onMounted, ref } from 'vue';
+
+onMounted(async () => {});
+
+const camera = ref();
+
+const snapshot = async () => {
+  const blob = await camera.value?.snapshot();
+
+  // To show the screenshot with an image tag, create a url
+  const url = URL.createObjectURL(blob);
+  console.log(url);
+};
+
+const loading = () => console.log('Camera is loading and will start any second');
+const started = () => console.log('Camera has started');
+const stopped = () => console.log('Camera has stopped');
+const paused = () => console.log('Video feed has paused');
+const resumed = () => console.log('Video feed has resumed');
+const cameraChange = (deviceID) => console.log(`Camera has been changed to ${deviceID}`);
+// const snapshot = (blob) => console.log('A snapshot has been taken');
+const error = (error) => console.log('error');
+</script>
